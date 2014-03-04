847b73e6956f362215f31ab83cf9544c791de2de
Craig Booth <cbooth@narrativescience.com>
2014-03-04 14:47:07 -0600

    Look, I can recurse on the car of a list!

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Calling eval-buffer on any of the ``chapter*.el`` files should print the results
    * Use defun instead of ``(define (lambda ()))``
    * Emacs lisp doesn't have 'else', instead make the final question in a cond something else that always evaluates to true, i.e. 't'
    * Added docstrings
+
+
+;; When recursing over two lists there are four termination conditions to
+;; consider:
+;;    * Both lists are null
+;;    * The first list is null
+;;    * The second list is null
+;;    * Neither list is null
+;;
+;; If we can guarantee the lists are the same length then only need to do
+;; two of these.
--- chapter5.el
+++ chapter5.el
@@ -3,3 +3,25 @@
 ;; Examples from the 4th Edition of The Little Schemer translated into
 ;; Emacs lisp.
 ;;
+
+(defun rember* (a ll)
+"Recursively traverse nested lists, removing any element that equals the element a"
+  (cond
+   ((null ll) '())                            ;if ll is null, return '()
+   ((atom (car ll))                           ;elif (car ll) is an atom
+     (cond
+      ((eq (car ll) a) (rember* a (cdr ll)))
+      (t (cons (car ll) (rember* a (cdr ll))))
+   ))
+   (t                                         ;else (car ll) is not an atom
+    (cons (rember* a (car ll))
+          (rember* a (cdr ll)))
+   )))
+
+; ((coffee) ((tea)) (and (hick)))
+(message "%s" (rember* 'cup '((coffee) cup ((tea) cup) (and (hick)) cup)))
+
+; (((tomato)) ((bean) (and ((flying)))))
+(message "%s" (rember* 'sauce '(((tomato sauce))
+                                ((bean sauce)
+                                (and ((flying)) sauce)))))
