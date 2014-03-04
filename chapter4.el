b693517cc3aa4c2dcc7cd1f7463e72a4f73c8691
Craig Booth <cbooth@narrativescience.com>
2014-03-03 23:18:42 -0600

    Completed chapter 4

--- chapter4.el
+++ chapter4.el
@@ -8,6 +8,7 @@
 (defun add1 (x) (+ x 1))
 (defun sub1 (x) (- x 1))
 
+;;In Lisp, zero? is zerop
 
 (defun o+ (n m)
 "Add n and m"
@@ -15,7 +16,7 @@
    ((zerop m) n)
    (t (add1 (o+ n (sub1 m))))))
 
-(message "%s" (o- 46 12))                        ; 58
+(message "%s" (o+ 46 12))                        ; 58
 
 
 (defun o- (n m)
@@ -42,22 +43,12 @@
 (defun mult (n m)
 "Multiply together m and n"
   (cond
-   ((zero m) 0)
+   ((zerop m) 0)
    (t (o+ n (mult n (sub1 m))))))
 
 (message "%s" (mult 5 3))                        ; 15
 (message "%s" (mult 13 4))                       ; 52
 
-
-;; When recursing over two lists there are four termination conditions to
-;; consider:
-;;    * Both lists are null
-;;    * The first list is null
-;;    * The second list is null
-;;    * Neither list is null
-;;
-;; If we can guarantee the lists are the same length then only need to do
-;; two of these.
 
 (defun tup+ (tup1 tup2)
 "Given two tuples of the same length, sum each element"
@@ -73,77 +64,165 @@
 
 (defun tup+ (tup1 tup2)
 "Given two tuples of any length, sum each element"
-)
+  (cond
+   ((and (null tup1) (null tup2))
+      '())
+   ((null tup1)
+      (cons (car tup2) (tup+ tup1 (cdr tup2))))
+   ((null tup2)
+      (cons (car tup1) (tup+ (cdr tup1) tup2)))
+   (t
+      (cons (o+ (car tup1) (car tup2))
+                (tup+ (cdr tup1) (cdr tup2))))))
 
-(message "%s" (tup+ '(3 6 9 11 4) '(8 5 2 0 7)))  ;(11 11 11 11 11)
-(message "%s" (tup+ '(2 3) '(4 6)))               ; 6 9
-;;add example (3 7 8 1) (4 6) = (7 15 8 1)
+(message "%s" (tup+ '(3 6 9 11 4) '(8 5 2 0 7)))  ; (11 11 11 11 11)
+(message "%s" (tup+ '(2 3) '(4 6)))               ; (6 9)
+(message "%s" (tup+ '(3 7 8 1) '(4 6)))           ; (7 13 8 1)
 
-(defun > (n m)
- "n > m?"
-;;(recurse over two numbers, case where neither is 0, n is 0, m is 0.
-;;order matters for equality)
-)
 
-;;e.g.'s are (> 12 133) = false and (> 120 11) = true
+(defun gt (n m)
+"n > m?"
+  (cond
+   ((zerop n) nil)
+   ((zerop m) t)
+   (t (gt (sub1 n) (sub1 m)))))
 
-(defun < (n m)
+(message "%s" (gt 12 133))       ;nil
+(message "%s" (gt 120 11))       ;t
+(message "%s" (gt 12 12))        ;nil
 
-)
 
-;;< 8 3 and < 6 6 and < 4 6 = f f t
+(defun lt (n m)
+"n < m"
+  (cond
+   ((zerop m) nil)
+   ((zerop n) t)
+   (t (lt (sub1 n) (sub1 m)))))
 
-(defun = (n m)
-  ;;Do both fundamentally and with gt and lt
-)
+(message "%s" (lt 8 3))          ;nil
+(message "%s" (lt 6 6))          ;nil
+(message "%s" (lt 4 6))          ;t
+
+
+(defun == (n m)
+"n == m"
+  (cond
+   ((or (lt n m) (gt n m)) nil)
+   (t t)))
+
+(message "%s" (== 8 3))          ;nil
+(message "%s" (== 6 6))          ;t
+(message "%s" (== 4 6))          ;nil
 
 
 (defun pow (n m)
+"n^m"
+  (cond
+   ((== m 0) 1)
+   (t (mult n (pow n (sub1 m))))))
 
-)
+(message "%s" (pow 1 1))         ;1
+(message "%s" (pow 2 3))         ;8
+(message "%s" (pow 5 3))         ;125
 
 
-(defun / (n m)
+(defun div (n m)
+"n / m"
+  (cond
+   ((lt n m) 0)
+   (t (add1 (div (o- n m) m)))))
 
-)
+(message "%s" (div 15 4))
+
 
 (defun length (lat)
+"Return the number of atoms in the list lat"
+  (cond
+   ((null lat) 0)
+   (t (add1 (length (cdr lat))))))
 
-)
+(message "%s" (length '(hotdogs with mustard sauerkraut and pickles)))  ; 6
 
-(defun pick (lat)
 
-)
+(defun pick (lat n)
+"Return the nth element from lat"
+  (cond
+   ((== (sub1 n) 0) (car lat))
+   (t (pick (cdr lat) (sub1 n)))))
 
-(defun rempick (lat)
+(message "%s" (pick '(lasagna spaghetti ravioli macaroni meatball) 4))   ; macaroni
 
-)
 
-(defun number (a)
+(defun rempick (n lat)
+"Remove the item from lat at position n"
+  (cond
+   ((null lat) '())
+   ((== (sub1 n) 0) (cdr lat))
+   (t (cons (car lat)
+      (rempick (sub1 n) (cdr lat))))))
 
-)
+(message "%s" (rempick 3 '(hotdogs with hot mustard)))    ; hotdogs with mustard
+
+
+;;In Lisp, number? is numberp
+
 
 (defun no-nums (lat)
+"Remove all numbers from a lat"
+  (cond
+   ((null lat) '())
+   ((numberp (car lat)) (no-nums (cdr lat)))
+   (t (cons (car lat) (no-nums (cdr lat))))))
 
-)
+(message "%s" (no-nums '(5 pears 6 prunes 9 dates)))      ; (pears prunes dates)
+
 
 (defun all-nums (lat)
+"Extract a tup from a lat by extracting all the numbers"
+  (cond
+   ((null lat) '())
+   ((numberp (car lat)) (cons (car lat) (all-nums (cdr lat))))
+   (t (all-nums (cdr lat)))))
 
-)
+(message "%s" (all-nums '(5 pears 6 prunes 9 dates)))     ; (5 6 9)
+
 
 (defun eqan (a1 a2)
+"true if two arguments are the same atom"
+  (cond
+   ((and (numberp a1) (numberp a2)) (= a1 a2))
+   ((numberp a1) nil)
+   ((numberp a2) nil)
+   (t (eq a1 a2))))
 
-)
+(message "%s" (eqan 1 1))
+(message "%s" (eqan 1 2))
+(message "%s" (eqan 3 'r))
+(message "%s" (eqan 1 '3))
 
 
 (defun occur (a lat)
+"count occurrences of a in lat"
+  (cond
+   ((null lat) 0)
+   ((eq a (car lat))
+      (add1 (occur a (cdr lat))))
+   (t
+      (occur a (cdr lat)))))
 
-)
+(message "%s" (occur 'cc '(cc bb cc bb cc)))       ;3
 
-(defun one (a lat)
 
-)
+(defun one (n)
+"true if n is 1, false otherwise"
+  (eq n 1))
 
 (defun rempick (n lat)
-"rewritten to use one"
-)
+"Remove the item from lat at position n, using function one"
+  (cond
+   ((null lat) '())
+   ((one n) (cdr lat))
+   (t (cons (car lat)
+      (rempick (sub1 n) (cdr lat))))))
+
+(message "%s" (rempick 3 '(hotdogs with hot mustard)))    ; hotdogs with mustard
---
+++ chapter5.el
@@ -0,0 +1,5 @@
+;; Chapter 5. *Oh My Gawd* It's Full of Stars
+;;
+;; Examples from the 4th Edition of The Little Schemer translated into
+;; Emacs lisp.
+;;
