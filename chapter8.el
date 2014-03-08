427ef789159bd64d85238bc6a24b93640b7a5136
Craig Booth <cbooth@narrativescience.com>
2014-03-08 13:51:56 -0600

    Blank files for chapter 8

---
+++ chapter8.el
@@ -0,0 +1,8 @@
+;; Chapter 8. Lambda the Ultimate
+;;
+;; Examples from the 4th Edition of The Little Schemer translated into
+;; Emacs lisp.
+;;
+(add-to-list 'load-path ".")
+(load "chapter2.el")
+(load "chapter3.el")
---
+++ chapter8_examples.el
@@ -0,0 +1,7 @@
+;; Chapter 8. Lambda the Ultimate (Examples)
+;;
+;; Examples from the 4th Edition of The Little Schemer translated into
+;; Emacs lisp.
+;;
+(add-to-list 'load-path ".")
+(load "chapter8.el")
